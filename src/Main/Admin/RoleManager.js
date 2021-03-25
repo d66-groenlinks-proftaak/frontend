5ba71fe9c7f5b0dbe8820f6b1fd7320dbd260da9
Jasper Meiracker <362948@student.fontys.nl>
2021-03-25 10:05:58 +0100

    .

---
+++ src/Main/Admin/RoleManager.js
@@ -0,0 +1,20 @@
+import React from "react";
+import {Route, Switch, Redirect} from "react-router-dom";
+import {login} from "../../Core/Authentication/authentication.actions";
+import {useSelector, connect} from "react-redux";
+import {
+    getAuthError,
+    getAuthAuthenticating
+} from "../../Core/Authentication/authentication.selectors";
+
+function RoleManager() {
+    return <div>
+        
+    </div>
+}
+ 
+const mapStateToProps = (state) => {
+    return {error: getAuthError(state), loggingIn: getAuthAuthenticating(state)}
+}
+
+export default connect(mapStateToProps)(RoleManager);
