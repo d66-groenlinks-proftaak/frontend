fdc2c4ff728978dea6a19231ac12d2e6c241c349
nick van Oijen <I468166@fhict.nl>
2021-04-08 11:02:11 +0200

    feature: added windowselecter in adminpanel

--- src/Main/Admin/Adminpanel.js
+++ src/Main/Admin/Adminpanel.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import {Route, Switch, Redirect} from "react-router-dom";
 import {login} from "../../Core/Authentication/authentication.actions";
 import {useSelector, connect} from "react-redux";
@@ -9,13 +9,36 @@ import {
 import Categories from "./Categories";
 import RoleManager from "./RoleManager";
 import ShadowBans from "./ShadowBan/ShadowBans";
-
+import { Menu } from 'primereact/menu';
 function AdminPanel (){
-    return <div style={{width: "100%"}}>
-        <Categories></Categories>
-        <RoleManager></RoleManager>
-        <ShadowBans style={{width: "100%"}}></ShadowBans>
+
+    const [window, setWindow] = useState("report");
+
+    let items = [
+        {label: 'Gerapporteede berichten', command: (e) =>{
+                setWindow("report")
+            }},
+        {label: 'Categorieën', command: (e) =>{
+                setWindow("catergorie")
+            }},
+        {label: 'Rollen Beheren' , command: (e) =>{
+                setWindow("rollen")
+            }}
+    ];
+    return <div className={"p-col-8 max-width"} style={{marginTop: "10px"}}>
+        <div className={"p-grid max-width"}>
+            <div className={"p-col-2"}>
+                <Menu className={"admin-menu"} model={items}></Menu>
+            </div>
+
+            <div className={"p-col-10"}>
+                {window !== undefined && window === "report" ? <ShadowBans className={"max-width"}></ShadowBans>: <span/>}
+                {window !== undefined && window === "catergorie" ? <Categories></Categories>: <span/>}
+                {window !== undefined && window === "rollen" ? <RoleManager></RoleManager>: <span/>}
+            </div>
+        </div>
     </div>
+
 }
 
 
