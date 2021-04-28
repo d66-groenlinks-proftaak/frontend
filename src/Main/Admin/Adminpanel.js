5bb371fd68ac1e79c742586621ebf090e5b55120
nickonos <70724339+nickonos@users.noreply.github.com>
2021-04-28 15:21:49 +0200

    feature: added global permissions and added access to adminpanel only as admin

--- src/Main/Admin/Adminpanel.js
+++ src/Main/Admin/Adminpanel.js
@@ -7,7 +7,12 @@ import Categories from "./Categories";
 import RoleManager from "./RoleManager/RoleManager";
 import ShadowBans from "./ShadowBan/ShadowBans";
 import { Menu } from 'primereact/menu';
-function AdminPanel (){
+import {getPermissions} from "../../Core/Global/global.selectors";
+import {connect} from "react-redux";
+import {getAuthAuthenticating, getAuthError} from "../../Core/Authentication/authentication.selectors";
+import {Redirect, Route} from "react-router-dom";
+
+function AdminPanel (props){
 
     const [window, setWindow] = useState("report");
 
@@ -22,7 +27,11 @@ function AdminPanel (){
                 setWindow("rollen")
             }}
     ];
-    return <div className={"p-col-12 p-grid p-justify-center"}>
+    console.log(props.permissions);
+    if(!props.permissions.includes(0))
+        return <Redirect to="/" />
+    return<div>
+        <div className={"p-col-12 p-grid p-justify-center"}>
         <div className={"p-col-8"} style={{marginTop: "10px"}}>
         <div className={"p-grid"}>
             <div className={"p-col-2"}>
@@ -37,9 +46,13 @@ function AdminPanel (){
         </div>
     </div>
     </div>
-
+</div>
                 
                 
 }
 
-export default AdminPanel;
+const mapStateToProps = (state) => {
+    return {error: getAuthError(state), loggingIn: getAuthAuthenticating(state), permissions: getPermissions(state)}
+}
+
+export default connect(mapStateToProps)(AdminPanel);
