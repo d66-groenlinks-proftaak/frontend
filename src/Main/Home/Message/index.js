dcf0971f963c7a940b8ada03ad16e0576f665109
nickonos <70724339+nickonos@users.noreply.github.com>
2021-05-20 10:06:06 +0200

    feature: added edit panel

--- src/Main/Home/Message/index.js
+++ src/Main/Home/Message/index.js
@@ -12,7 +12,7 @@ import {Dialog} from 'primereact/dialog';
 import {Tooltip} from 'primereact/tooltip';
 import {ScrollTop} from 'primereact/scrolltop';
 import Report from "./Report";
-import {getAuthAuthenticated} from "../../../Core/Authentication/authentication.selectors";
+import {getAuthAuthenticated, getAuthId} from "../../../Core/Authentication/authentication.selectors";
 import {connect} from "react-redux";
 import {WindowScroller} from "react-virtualized";
 import Replies from "./Replies";
@@ -21,6 +21,8 @@ import Header from "./Header";
 import CreateReply from "./CreateReply";
 import {setReplyOpen, setReplyingToId, setReplyingTo, setReplies} from "../../../Core/Message/message.actions";
 import {getReplyOpen} from "../../../Core/Message/message.selectors";
+import {FileUpload} from "primereact/fileupload";
+
 
 function Message(props) {
     const [author, setAuthor] = useState("");
@@ -35,6 +37,15 @@ function Message(props) {
     const [attachments, setAttachments] = useState([]);
     const [showAttachmentState, setShowAttachment] = useState(false);
     const [attachment, setAttachment] = useState("");
+    const [editWindow, setEditWindow] = useState(false);
+    const [invalidContent, setInvalidContent] = useState(false);
+    const [invalidTitle, setInvalidTitle] = useState(false);
+    const [additionalProps, setAdditionalProps] = useState({});
+    const [uploadRef, setUploadRef] = useState();
+    const [editMessageContent, setEditMessageContent] = useState({});
+    const [editMessageTitle, setEditMessageTitle] = useState({});
+
+
 
     const menuRef = React.createRef();
 
@@ -45,13 +56,7 @@ function Message(props) {
             setReportWindow(true);
         }
     },
-        {
-            label: "Bewerken",
-            icon: "pi pi-pencil",
-            command: () => {
-                props.connection.send();
-            }
-        },
+
         {
             label: "Lock",
             icon: "pi pi-lock",
@@ -94,6 +99,94 @@ function Message(props) {
         setShowAttachment(false);
     }
 
+    const onInputChanged = (type, content) => {
+        validateInput(type, content)
+
+/*        this.setState(oldState => {
+            const newPost = oldState.newPost;
+            newPost[type] = content;
+
+            return {newPost}
+        })*/
+        if(type === "content"){
+            setEditMessageContent(content)
+        }
+        else if (type === "title"){
+            setEditMessageTitle(content)
+        }
+
+    }
+
+    const validateInput = (type, content, cb) => {
+        console.log(type,content)
+        if (content === undefined)
+            return;
+
+        if (type === "title") {
+            if (content.length > 40) {
+                setInvalidTitle("De title is te lang")
+            } else if (content.length <= 5) {
+                setInvalidTitle("De titel is te kort")
+            } else {
+                setInvalidTitle(false)
+            }
+        }
+
+        if (type === "content") {
+            if (content.length > 2000) {
+                setInvalidContent("De tekst is te lang")
+            } else if (content.length <= 10) {
+                setInvalidContent("De tekst is te kort")
+            } else {
+                setInvalidContent(false)
+            }
+        }
+    }
+
+    const createPost = () => {
+        console.log("CREATE")
+        validateInput("title", editMessageTitle, () => {
+            validateInput("content", editMessageContent, () => {
+                if (this.props.loggedIn && !invalidTitle&& !invalidContent) {
+                    uploadRef.upload();
+                        } else if (!invalidTitle && !invalidContent) {
+                            uploadRef.upload();
+                        }
+                    });
+                });
+    }
+
+    const upload = (files, ref) => {
+        let formData = new FormData();
+
+        for(let i in files.files) {
+            formData.append(files.files[i].name, files.files[i]);
+        }
+
+        setAdditionalProps({
+                disabled: true,
+                icon: "pi pi-spin pi-spinner"
+        })
+
+        formData.append("Title", this.state.newPost.title);
+        formData.append("Content", this.state.newPost.content);
+        formData.append("Email", this.state.newPost.email);
+        formData.append("Author", this.state.newPost.author);
+        formData.append("Token", this.props.token);
+
+        fetch('http://localhost:5000/message/create', {
+            method: 'POST',
+            body: formData
+        }).then(() => {
+            setAdditionalProps({})
+
+            ref.clear();
+            setEditWindow(false);
+        }).catch((e) => {
+            console.log(e);
+        })
+    }
+
     useEffect(() => {
         props.connection.on("SendThreadDetails", thread => {
             setAuthor(thread.parent.author);
@@ -122,12 +215,73 @@ function Message(props) {
             <LoadingMessage/>
         </div>
     }
+    console.log(authorId, props.accountId)
+
+
+
+    if(authorId === props.accountId){
+        extraOptions.push({
+            label: "Bewerken",
+            icon: "pi pi-pencil",
+            command: () => {
+                setEditWindow(true)
+            }
+        },)
+    }
+
 
     return <div className={"p-mt-5"}>
         <Menu ref={menuRef} popup model={extraOptions}/>
 
         <Header/>
 
+        <Sidebar className={"p-col-12 new-post p-grid p-justify-center p-nogutter"}
+                 style={{overflowY: "scroll", overflowX: "hidden", width: "100%"}}
+                 position="bottom"
+                 showCloseIcon={false}
+                 visible={editWindow} onHide={() => setEditWindow(false)}>
+            <div className="new-post-content p-p-3 p-pt-3">
+                <InputText style={{width: "100%"}} placeholder={"Titel"}
+                           className={invalidTitle ? "p-invalid" : ""}
+                           value={title} onChange={e => {
+                    onInputChanged("title", e.target.value)
+                }}/>
+                <div style={{color: "red"}}>{invalidTitle ? invalidTitle :
+                    <span>&nbsp;</span>}</div>
+
+                <Editor placeholder={"Typ hier uw bericht"} modules={{
+                    toolbar: [[{'header': 1}, {'header': 2}], ['bold', 'italic'], ['link']]
+                }} className={invalidContent ? "p-invalid" : ""}
+                        style={{height: '250px'}}
+                        value={content} onTextChange={(e) => {
+                    console.log(e)
+                    onInputChanged("content", e.htmlValue)
+                }}/>
+
+                <div style={{color: "red"}}>{invalidContent ? invalidContent:
+                    <span>&nbsp;</span>}</div>
+
+                <b>Voeg maximaal 2 bestanden toe</b>
+                <FileUpload ref={(ref) => {
+                    setUploadRef(ref)
+                }}  customUpload={true} uploadHandler={(files) => {
+                    upload(files, uploadRef);
+                }} chooseLabel="Bestanden Kiezen" name="demo[]" url="./upload" multiple />
+
+                <div>
+                    <Button {...additionalProps} iconPos={"left"} icon={"pi pi-plus"}
+                            onClick={() => {
+                                createPost()
+                            }} label={"Plaatsen"}/>
+                    <Button {...additionalProps}
+                            className={"p-button-secondary p-button-outlined p-ml-3"}
+                            iconPos={"right"}
+                            onClick={() => {
+                                setEditWindow(false)
+                            }} label={"Annuleren"}/>
+                </div>
+            </div>
+        </Sidebar>
         <Thread togglePostWindow={togglePostWindow} attachments={attachments}
                 showAttachment={(a) => {
                     showAttachment(true, a)
@@ -188,7 +342,7 @@ function Message(props) {
 
 const
     mapStateToProps = (state) => {
-        return {loggedIn: getAuthAuthenticated(state), replyOpen: getReplyOpen(state)}
+        return {loggedIn: getAuthAuthenticated(state), replyOpen: getReplyOpen(state), accountId: getAuthId(state)}
     }
 
 export default connect(mapStateToProps)
